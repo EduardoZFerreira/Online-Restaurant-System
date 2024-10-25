b57d5120ff082d557b66511a9cfa27e7d9b135c6
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-25 00:55:09 -0300

    Fetching menu items list to component

--- frontend/src/Views/MenuItems/MenuItemsList.tsx
+++ frontend/src/Views/MenuItems/MenuItemsList.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useGetMenuItemsQuery } from "../../api/api";
 
 const MenuItemsList = () => {
-  return <></>;
+  const { data, isLoading, isSuccess } = useGetMenuItemsQuery();
+
+  return (
+    <>
+      <h1>Cardápio</h1>
+      {isLoading && <h1>Loading...</h1>}
+      {isSuccess &&
+        data.map((category) => (
+          <>
+            <h1>{category.title}</h1>
+            <ul>
+              {category.menuItems.map((menuItem) => (
+                <li>
+                  <img src={menuItem.imageUrl} width="50px" height="50px" />
+                  <h2>{menuItem.title}</h2>
+                  <p>{menuItem.description}</p>
+                  <span>R$ {menuItem.value.toString().replace(".", ",")}</span>
+                </li>
+              ))}
+            </ul>
+          </>
+        ))}
+    </>
+  );
 };
 
 export default MenuItemsList;
