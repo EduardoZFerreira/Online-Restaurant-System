c47b20a040763c0a3cc614eb108ee4fc6ca3c523
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-25 00:39:29 -0300

    Configuring redux store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
-import { ApiProvider } from "@reduxjs/toolkit/query/react";
-import { api } from "./api/api.ts";
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ApiProvider api={api}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ApiProvider>
+    </Provider>
   </StrictMode>
 );
