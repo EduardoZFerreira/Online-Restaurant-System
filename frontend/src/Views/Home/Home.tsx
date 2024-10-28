cb0bbf5988d8db8b611a1a976960d4bb6c8738c8
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-27 23:10:51 -0300

    testing netlify deploy

--- frontend/src/Views/Home/Home.tsx
+++ frontend/src/Views/Home/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
     <>
       <div className="home-container">
         <div className="background-container"></div>
-        <h1 className="display-1">Bem vindo!</h1>
+        <h1 className="display-1">Seja bem vindo!</h1>
       </div>
     </>
   );
