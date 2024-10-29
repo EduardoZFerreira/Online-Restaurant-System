c3500c4a744b4261a5f445fb494c328614f2b74a
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-29 01:41:47 -0300

    Fetching users and setting reservations to lazy fetching

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,12 +2,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { IMenuItemCategory } from "../Interfaces/IMenuItemCategory";
 import { SaveReservationDTO } from "../DTOs/SaveReservationDTO";
 import { IReservation } from "../Interfaces/IReservation";
+import { IUser } from "../Interfaces/IUser";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_URL ?? "http://localhost:8081",
   }),
+  tagTypes: ["Reservation"],
   endpoints: (builder) => ({
+    getUsers: builder.query<IUser[], void>({
+      query: () => "/user",
+    }),
     getMenuItems: builder.query<IMenuItemCategory[], void>({
       query: () => "/menu",
     }),
@@ -17,6 +22,7 @@ export const api = createApi({
         method: "POST",
         body: reservation,
       }),
+      invalidatesTags: ["Reservation"],
     }),
     getReservationByUserId: builder.query<IReservation[], string>({
       query: (userId) => `/reservation/user/${userId}`,
@@ -25,7 +31,8 @@ export const api = createApi({
 });
 
 export const {
+  useGetUsersQuery,
   useGetMenuItemsQuery,
   useAddReservationMutation,
-  useGetReservationByUserIdQuery,
+  useLazyGetReservationByUserIdQuery,
 } = api;
