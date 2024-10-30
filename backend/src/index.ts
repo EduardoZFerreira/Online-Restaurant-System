44096f74e978fb81fc3118405be5e75cb627dbac
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-30 00:51:26 -0300

    Installing dependencies for JWT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import { routes } from "./routes";
 import cors from "cors";
 import bodyParser from "body-parser";
 import swaggerUI from "swagger-ui-express";
+import cookieParser from "cookie-parser";
 
 const swaggerFile = require("./docs/swagger-output.json");
 
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.use(routes);
 app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
