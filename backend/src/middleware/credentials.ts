e29c4f1bda33cd8bf543fb2664391a36fe4aae11
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-31 22:01:19 -0300

    Moving allowed origins inside the method, so the environment variables can be accessed

--- backend/src/middleware/credentials.ts
+++ backend/src/middleware/credentials.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
-const allowedOrigins = process?.env?.ALLOWED_ORIGINS?.split(" ");
-
 const credentials = (req: Request, res: Response, next: any) => {
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(" ");
   const origin = req.headers.origin;
+
   if (origin && allowedOrigins?.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", "true");
   }
