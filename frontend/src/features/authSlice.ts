a9902a1ae897d1623d9ca0e375f41f2772355bf8
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-11-01 00:53:58 -0300

    getting the name in the auth slice

--- frontend/src/features/authSlice.ts
+++ frontend/src/features/authSlice.ts
@@ -2,15 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { user: null, token: null, roles: [] },
+  initialState: { name: null, user: null, token: null, roles: [] },
   reducers: {
     setCredentials: (state, action) => {
-      const { user, token, roles } = action.payload;
+      const { name, user, token, roles } = action.payload;
+      state.name = name;
       state.user = user;
       state.token = token;
       state.roles = roles;
     },
     logOut: (state) => {
+      state.name = null;
       state.user = null;
       state.token = null;
       state.roles = [];
@@ -22,6 +24,7 @@ export const { setCredentials, logOut } = authSlice.actions;
 
 export default authSlice.reducer;
 
+export const selectCurrentName = (state: any) => state.auth.name;
 export const selectCurrentUser = (state: any) => state.auth.user;
 export const selectCurrentToken = (state: any) => state.auth.token;
 export const selectCurrentRoles = (state: any) => state.auth.roles;
